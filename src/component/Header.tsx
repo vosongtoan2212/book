9443bbe43b68d99344147465a8ac37da1d811c60
Vo Song Toan <vosongtoan21@gmail.com>
2024-02-19 22:02:18 +0700

    init source

---
+++ src/component/Header.tsx
@@ -0,0 +1,10 @@
+
+export default function Header() {
+  return (
+    <header className="">
+        <div className="bg-primary w-full h-[56px] text-primary">
+            Top
+        </div>
+    </header>
+  );
+}
